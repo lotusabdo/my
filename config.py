b6b12e040947a7de1b30ca78f8c1b5265273a12b
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-04-09 03:07:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/lotusabdo/meshoe.git",
+    "https://github.com/lotusabdo/AbdoX.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(

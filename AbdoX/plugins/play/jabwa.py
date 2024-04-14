4f51430e652aa10644485fd9c2f9d3371d858fdf
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-04-14 19:33:25 +0200

    Add files via upload

---
+++ AbdoX/plugins/play/jabwa.py
@@ -0,0 +1,32 @@
+import asyncio
+from pyrogram import Client, filtersغ
+from strings import get_command
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from AbdoX import (Apple, Resso,Spotify, Telegram, YouTube, app)
+
+
+@app.on_message(
+     command(["/help", "الاوامر"])
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/73299cc44862f1ec277dd.jpg",
+caption=f"""[𝐒𝐎𝐔𝐑𝐂𝐄 𝐁𝐎𝐃𝐀](https://t.me/l2_2Y)""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                InlineKeyboardButton(
+                    "عربي 🇪🇬", callback_data="arbic"
+                ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "English 🇺🇲", callback_data="english"),
+                ],
+            ]
+        ),
+    )
+
+

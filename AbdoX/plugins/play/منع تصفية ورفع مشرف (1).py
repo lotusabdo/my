c53105f7c33df1daa28306b657d956076a0c2fbb
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-04-27 09:56:42 +0300

    Update منع تصفية ورفع مشرف (1).py

--- AbdoX/plugins/play/منع تصفية ورفع مشرف (1).py
+++ AbdoX/plugins/play/منع تصفية ورفع مشرف (1).py
@@ -4,7 +4,7 @@ from AbdoX import app
 import random
 from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-▒▒
+
 
 iddof = []
 @app.on_message(

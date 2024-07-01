76e0b0dd8846a822aefe028c52ab68c0d89eb11f
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-07-01 13:25:47 +0300

    Add files via upload

---
+++ AbdoX/plugins/play/cmds.py
@@ -0,0 +1,44 @@
+import asyncio
+import os
+import requests
+import pyrogram
+from pyrogram import Client, filters, emoji
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from pyrogram.errors import MessageNotModified
+from AbdoX import app
+from config import OWNER_ID, LOGGER_ID
+
+
+@app.on_message(command(["الاوامر"]))
+async def zdatsr(client: Client, message: Message):
+    usr = await client.get_users(OWNER_ID)
+    name = usr.first_name
+    usrnam = usr.username
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/0395107f7b3d1e6ddda9f.jpg",
+        caption=f"""<b>↯︙مرحباً بك عزيزي</b>\n<b>↯︙استخدم الازرار بالاسفل\n» ل تصفح اوامر الميوزك</b>""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "‹ اوامر التشغيل ›", callback_data="zzzll"),
+                ],[
+                    InlineKeyboardButton(
+                        "‹ اوامر القناة ›", callback_data="zzzch"),
+                    InlineKeyboardButton(
+                        "‹ اوامر الادمن ›", callback_data="zzzad"),
+                ],[
+                    InlineKeyboardButton(
+                        "‹ اوامر المطور ›", callback_data="zzzdv"),
+                ],[
+                    InlineKeyboardButton(name, url=f"https://t.me/{usrnam}"),
+                ],[
+                    InlineKeyboardButton(
+                        "‹ 𝐒𝐎𝐔𝐑𝐂𝐄 𝐒𝐀𝐕𝐈𝐎 ›", url="https://t.me/l2_2Y"),
+                ],
+            ]
+        ),
+    )
+
+

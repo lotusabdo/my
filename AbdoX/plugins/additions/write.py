7b2c2da8fdd58fc90316d6d624f82d903f2d27de
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-07-02 02:04:49 +0300

    Update write.py

--- AbdoX/plugins/additions/write.py
+++ AbdoX/plugins/additions/write.py
@@ -1,6 +1,5 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from config import  BOT_USERNAME
 from AbdoX import app
 import requests
 

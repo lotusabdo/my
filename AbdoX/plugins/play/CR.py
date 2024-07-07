ca512aa61e52e021163946a266e2a19928615874
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-07-07 10:36:54 +0300

    Update CR.py

--- AbdoX/plugins/play/CR.py
+++ AbdoX/plugins/play/CR.py
@@ -75,29 +75,3 @@ async def deev(client: Client, message: Message):
        os.remove(photo)
      except:
         pass
-
-@app.on_message(filters.command(["مبرمج السورس"], ""), group=73) 
-async def deev(client: Client, message: Message):
-     user = await client.get_chat(chat_id="Y_o_V")
-     name = user.first_name
-     username = user.username 
-     bio = user.bio
-     user_id = user.id
-     photo = user.photo.big_file_id
-     photo = await client.download_media(photo)
-     link = f"https://t.me/{message.chat.username}"
-     title = message.chat.title if message.chat.title else message.chat.first_name
-     chat_title = f"User : {message.from_user.mention} \nChat Name : {title}" if message.from_user else f"Chat Name : {message.chat.title}"
-     try:
-      await client.send_message(username, f"هناك شخص بالحاجه اليك عزيزي المطور\n{chat_title}\nChat Id : `{message.chat.id}`",
-      reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{title}", url=f"{link}")]]))
-     except:
-       pass
-     await message.reply_photo(
-     photo=photo,
-     caption=f"𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚛 𝙽𝚊𝚖𝚎 : {name} \n𝚍𝚎𝚟 𝚞𝚜𝚎𝚛 𝚗𝚊𝚖𝚎 : @{username}\n{bio}",
-     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{name}", user_id=f"{user_id}")]]))
-     try:
-       os.remove(photo)
-     except:
-        pass

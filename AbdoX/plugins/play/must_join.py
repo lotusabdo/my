d5bca260f17690e9f165361570e11404b7cc1ca0
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-08-03 22:57:31 +0300

    Add files via upload

---
+++ AbdoX/plugins/play/must_join.py
@@ -0,0 +1,31 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from AbdoX import app
+
+@app.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(bot: Client, msg: Message):
+    if not "https://t.me/l2_2Y":  # Not compulsory
+        return
+    try:
+        try:
+            await bot.get_chat_member("l2_2Y", msg.from_user.id)
+        except UserNotParticipant:
+            if "https://t.me/l2_2Y".isalpha():
+                link = "https://t.me/l2_2Y"
+            else:
+                chat_info = await bot.get_chat("l2_2Y")
+                link = chat_info.invite_link
+            try:
+                await msg.reply(
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @l2_2Y .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("سورس", url=link)]
+                    ])
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"I m not admin in the MUST_JOIN chat @l2_2Y !")
